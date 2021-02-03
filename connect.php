2dc19f0f97c4c48ca358013d703f296b5f29caf0
niccolobruscantini <75781602+niccolobruscantini@users.noreply.github.com>
2021-02-03 12:26:04 +0100

    Add files via upload
    Abbiamo provato tramite altervista , il collegamento con il database per la funzionalità del sign up con la decriptazione della password con md5 nel database

---
+++ connect.php
@@ -0,0 +1,22 @@
+<?php
+
+$nome = filter_input(INPUT_POST, 'nome');
+$cognome = filter_input(INPUT_POST, 'cognome');
+$email = filter_input(INPUT_POST, 'email');
+$psw = filter_input(INPUT_POST, 'password');
+$psw = md5($psw);
+	$host = "localhost";
+    $usarneme="nicoasciutti19";
+    $password="";
+    $nomedb="my_nicoasciutti19";
+    $miodb= new mysqli($host,$usarneme,$password,$nomedb);
+
+    if ($miodb->connect_errno>0) {
+            die('Errore di connessione (' . $miodb->connect_errno . ')' . $miodb->connect_error);
+        }
+    $sql = "Insert INTO Login (Nome,Cognome,Email,Password) values ('$nome','$cognome','$email','$psw')";
+    $miodb->query($sql); 
+    $miodb->close();
+    header("Refresh:0; url=index.html");
+	
+?>
